afeb19348cb430cc4616c8e6c967fac926b6edf3
gelald <yb.ng@foxmail.com>
2022-08-21 14:47:14 +0800

    rocketmq-client 批量发送消息

--- rocketmq-client/rocketmq-producer-client/src/main/java/com/github/gelald/rocketmq/producer/client/controller/DefaultProducerController.java
+++ rocketmq-client/rocketmq-producer-client/src/main/java/com/github/gelald/rocketmq/producer/client/controller/DefaultProducerController.java
@@ -1,14 +1,12 @@
 package com.github.gelald.rocketmq.producer.client.controller;
 
 import com.github.gelald.rocketmq.common.constant.RocketMQConstant;
-import com.github.gelald.rocketmq.producer.client.configuration.RocketMQBaseProducerConfiguration;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
-import org.apache.rocketmq.client.producer.MQProducer;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
@@ -19,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -99,11 +98,16 @@ public class DefaultProducerController {
         return "send complete";
     }
 
-    @ApiOperation("查看有多少个生产者被创建了")
-    @GetMapping("/producer-count")
-    public Integer getProducerCount() {
-        List<MQProducer> mqProducers = RocketMQBaseProducerConfiguration.mqProducers;
-        return mqProducers.size();
+    @ApiOperation("批量发送消息")
+    @GetMapping("/batch-message")
+    public SendResult sendBatchMessage() throws MQBrokerException, RemotingException, InterruptedException, MQClientException {
+        List<Message> messages = new ArrayList<>(10);
+        for (int i = 1; i <= 10; i++) {
+            String messageBody = "测试批量发送消息第" + i + "条消息";
+            Message message = new Message((RocketMQConstant.TOPIC_PREFIX + "client"), "batch", messageBody.getBytes(StandardCharsets.UTF_8));
+            messages.add(message);
+        }
+        return this.defaultMQProducer.send(messages);
     }
 
     @Autowired
